4d8f6bf089c75aa43d950be2cb24e76978af5423
ColeStolt <stoltcol2000@gmail.com>
2023-04-27 11:05:54 -0500

    new_config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId:         'us-west-2_0uBoj0GVx',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '25ddkmj4v6hfsfvruhpfi7n4hv',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId:   '2quke9gjkapfl9qnf1kbe5ujd',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region:             'us-west-2'          // e.g. us-east-2
     },
     api: {
