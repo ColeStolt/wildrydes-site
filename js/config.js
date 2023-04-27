ab82da51d1d6e4519d14b4dd486de9b493076fe8
ColeStolt <stoltcol2000@gmail.com>
2023-04-27 18:00:44 -0500

    new_config

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region:             'us-west-2'          // e.g. us-east-2
     },
     api: {
-        invokeUrl:          'arn:aws:cognito-idp:us-west-2:778422445161:userpool/us-west-2_0uBoj0GVx'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          'https://nf1886tbh6.execute-api.us-west-2.amazonaws.com/prod'          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
